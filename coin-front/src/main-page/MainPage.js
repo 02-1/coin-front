677d7c2960e59e0454fe960ed6b4455affc39570
eunseo <jangeun4097@naver.com>
2024-05-03 09:10:12 +0900

    새로고침 구현

--- coin-front/src/main-page/MainPage.js
+++ coin-front/src/main-page/MainPage.js
@@ -2,29 +2,62 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./component/SearchBar";
 import TableList from "./component/TableList";
 import TableOfContents from "./component/TableOfContents";
-
+import "./css/MainPage.css";
 import Loading from "../common/component/Loading";
 import { getCoinList } from "./MainPageFetch";
 
 function MainPage() {
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [autoRefreshEnabled, setAutoRefreshEnabled] = useState(false);
 
   const getList = async () => {
-    const newList = await getCoinList();
-
-    console.log(newList);
-    setList(newList);
-    setLoading(false);
+    try{
+      const newList = await getCoinList();
+      setList(newList);
+      setLoading(false);
+    }catch{
+      console.log("서버 연결 안되는뎁..");
+      setLoading(false);
+    }
+    
   };
 
   useEffect(() => {
     getList();
-
   }, []);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (autoRefreshEnabled) {
+      intervalId = setInterval(() => {
+        setLoading(true);
+        getList();
+        console.log("새로고침")
+      }, 10000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [autoRefreshEnabled]);
+
+  const handleAutoRefreshToggle = () => {
+    setAutoRefreshEnabled(!autoRefreshEnabled);
+  };
 
   return (
     <>
+      <label>
+        <span>자동 새로고침</span>
+        <input
+          role="switch"
+          type="checkbox"
+          checked={autoRefreshEnabled}
+          onChange={handleAutoRefreshToggle}
+        />
+      </label>
       <SearchBar setList={setList} setLoading={setLoading} />
       <TableOfContents />
       {loading && <Loading />}
