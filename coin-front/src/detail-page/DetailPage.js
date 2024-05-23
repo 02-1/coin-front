b6f39ef034b7d8d87c9fbd3cf6e90de407bd8b37
jeonghyeon <3575253@naver.com>
2024-05-23 15:17:33 +0900

    코인 상세정보 페이지

--- coin-front/src/detail-page/DetailPage.js
+++ coin-front/src/detail-page/DetailPage.js
@@ -1,14 +1,40 @@
 import DefaultPage from "../common/default-page/DefaultPage";
 import { useLocation } from "react-router-dom";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./css/DetailPage.css";
 function DetailPage() {
   const location = useLocation();
   const { rowData } = location.state;
+  const [detail, setDetail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        `http://${process.env.REACT_APP_IP}/coin-detail?symbol=${rowData.ticker}`
+      );
+      setDetail(() => res.data.detail);
+    } catch (error) {
+      console.error("Error fetching details:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <>
-      <DefaultPage rowData={rowData}/>
-      details
+      <DefaultPage rowData={rowData} />
+      <div className="detail-container">
+        {loading ? <div>Loading...</div> : <div>{detail}</div>}
+      </div>
     </>
   );
 }
+
 export default DetailPage;
